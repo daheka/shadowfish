6430a4ca0cbda0916eb784bf18571632f8dec330
daheka <r.forkink@hotmail.com>
2017-02-08 22:04:46 +0100

    "New dao project"

---
+++ Dao/src/main/java/com/daheka/nl/social/shadowfish/dao/User.java
@@ -0,0 +1,47 @@
+package com.daheka.nl.social.shadowfish.dao;
+
+import javax.persistence.*;
+
+/**
+ * Created by daheka on 2/8/17.
+ */
+@Entity
+@Table(name="USER")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+    @Column(name="username", unique=true, length=60, nullable = false)
+    private String username;
+    @Column(name="password", length=60, nullable=false)
+    private String password;
+
+    public User() {
+        // Empty Constructor
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
