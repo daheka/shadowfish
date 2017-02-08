b579d092a1260e51d2102954a42287cd9fde3c13
daheka <r.forkink@hotmail.com>
2017-02-08 23:11:30 +0100

    Made first application connection to database

--- RestServer/src/main/java/com/daheka/nl/social/shadowfish/restserver/Application.java
+++ RestServer/src/main/java/com/daheka/nl/social/shadowfish/restserver/Application.java
@@ -8,12 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by daheka on 2/8/17.
  */
 @SpringBootApplication
+@Configuration
+@EntityScan(value = "com.daheka.nl.social.shadowfish")
 public class Application {
 
     private static final Logger log = (Logger) LoggerFactory.getLogger(Application.class);
@@ -25,35 +29,35 @@ public class Application {
         SpringApplication.run(Application.class);
     }
 
-    @Bean
-    public CommandLineRunner demo(final UserRepository repository) {
-        return (args) -> {
-            // save a couple of customers
-            repository.save(new User());
-            repository.save(new User());
-            repository.save(new User());
-
-            // fetch all customers
-            log.info("Users found with findAll():");
-            log.info("-------------------------------");
-            for (User user : repository.findAll()) {
-                log.info(user.toString());
-            }
-            log.info("");
-
-            // fetch an individual customer by ID
-            User user = repository.findOne(1L);
-            log.info("User found with findOne(1L):");
-            log.info("--------------------------------");
-            log.info(user.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customer found with findByUsername('daheka'):");
-            log.info("--------------------------------------------");
-            User user2 = repository.findByUsername("daheka");
-            log.info(user2.toString());
-            log.info("");
-        };
-    }
+//    @Bean
+//    public CommandLineRunner demo(final UserRepository repository) {
+//        return (args) -> {
+//            // save a couple of customers
+//            repository.save(new User("Ronald", "12336612"));
+//            repository.save(new User("Niels", "12336612"));
+//            repository.save(new User("Koen", "12336612"));
+//
+//            // fetch all customers
+//            log.info("Users found with findAll():");
+//            log.info("-------------------------------");
+//            for (User user : repository.findAll()) {
+//                log.info(user.toString());
+//            }
+//            log.info("");
+//
+//            // fetch an individual customer by ID
+//            User user = repository.findOne(1L);
+//            log.info("User found with findOne(1L):");
+//            log.info("--------------------------------");
+//            log.info(user.toString());
+//            log.info("");
+//
+//            // fetch customers by last name
+//            log.info("Customer found with findByUsername('daheka'):");
+//            log.info("--------------------------------------------");
+//            User user2 = repository.findByUsername("daheka");
+//            log.info(user2.toString());
+//            log.info("");
+//        };
+//    }
 }
