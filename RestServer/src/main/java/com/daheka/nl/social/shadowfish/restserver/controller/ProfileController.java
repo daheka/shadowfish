d4931d288fdecfa12d89a546bc4f1dbdffee8dd0
daheka <r.forkink@hotmail.com>
2017-02-13 19:59:27 +0100

    WIP

--- RestServer/src/main/java/com/daheka/nl/social/shadowfish/restserver/controller/ProfileController.java
+++ RestServer/src/main/java/com/daheka/nl/social/shadowfish/restserver/controller/ProfileController.java
@@ -2,6 +2,8 @@ package com.daheka.nl.social.shadowfish.restserver.controller;
 
 import ch.qos.logback.classic.Logger;
 import com.daheka.nl.social.shadowfish.dao.*;
+import com.daheka.nl.social.shadowfish.restserver.repository.AppUserRepository;
+import com.daheka.nl.social.shadowfish.restserver.repository.PersonRepository;
 import com.daheka.nl.social.shadowfish.restserver.repository.ProfileRepository;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,24 +19,28 @@ public class ProfileController {
     private static final Logger log = (Logger) LoggerFactory.getLogger(ProfileController.class);
 
     @Autowired
-    private ProfileRepository repository;
+    private ProfileRepository profileRepository;
+    @Autowired
+    private AppUserRepository appUserRepository;
+    @Autowired
+    private PersonRepository personRepository;
 
     @RequestMapping( value = "/profile", method = RequestMethod.GET )
     @ResponseBody
     public Iterable<Profile> findAllProfiles(){
-        return repository.findAll();
+        return profileRepository.findAll();
     }
 
     @RequestMapping(value="/profile/{id}", method=RequestMethod.GET)
     @ResponseBody
     public Profile findProfileById(@PathVariable(value="{id}") Long id) {
-        return repository.findOne(id);
+        return profileRepository.findOne(id);
     }
 
     @RequestMapping(value="/profile/{appUser}", method=RequestMethod.GET)
     @ResponseBody
     public Profile findProfileByUser(@PathVariable(value="{appUser}") AppUser user) {
-        return repository.findByAppUser(user);
+        return profileRepository.findByAppUser(user);
     }
 
     @RequestMapping(value="/profile", method=RequestMethod.POST)
@@ -48,11 +54,13 @@ public class ProfileController {
                                  @RequestParam(value="gender") Gender gender
                                  ) {
         Person person = new Person(firstname, lastname, age, gender);
+        person = personRepository.save(person);
         AppUser appUser = new AppUser(username, password);
+        appUser = appUserRepository.save(appUser);
         Profile profile = new Profile();
         profile.setPerson(person);
         profile.setUser(appUser);
-        return repository.save(profile);
+        return profileRepository.save(profile);
     }
 
 }
